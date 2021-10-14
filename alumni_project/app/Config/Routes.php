bac157d3ba3cb48826a2a2e23960a8699f0fb1f1
ntpoom9 <zerpoom01@gmail.com>
2021-10-15 01:46:00 +0700

    fix baseURL im App.php and FCPATH in index.php

--- alumni_project/app/Config/Routes.php
+++ alumni_project/app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+
 // Login
 $routes->get('login','Login::index');
 $routes->get('/','Login::logout');
